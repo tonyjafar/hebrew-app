992cc8e8716260f2ef90943bfc6c221ca6c5f62d
tony <thaer.m.jafar@gmail.com>
2024-07-16 17:54:02 +0200

    return 6 items

--- src/words_functions.ts
+++ src/words_functions.ts
@@ -2,8 +2,7 @@ import {words} from './words'
 
 
 function getRandomSlice<T>(array: T[]): T[] {
-    const maxLength = 6;
-    const sliceLength = Math.floor(Math.random() * Math.min(maxLength, array.length)) + 1;
+    const sliceLength = 6;
     const startIndex = Math.floor(Math.random() * (array.length - sliceLength + 1));
     return array.slice(startIndex, startIndex + sliceLength);
 }

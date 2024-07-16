ba3c9633829aa1818f4a09fe35406f2b8d800869
tony <thaer.m.jafar@gmail.com>
2024-07-16 20:37:39 +0200

    add aws infra

---
+++ aws-hebrew-infra/lib/aws-hebrew-infra-stack.ts
@@ -0,0 +1,75 @@
+import * as dotenv from 'dotenv';
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { aws_cloudfront as cf } from 'aws-cdk-lib';
+import { aws_cloudfront_origins as origins } from 'aws-cdk-lib';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
+import { aws_s3_deployment as depl } from 'aws-cdk-lib';
+import { aws_certificatemanager as crt } from 'aws-cdk-lib';
+import { aws_route53 as route } from "aws-cdk-lib";
+import path = require('path');
+
+
+export class AwsHebrewInfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    dotenv.config();
+    const certArn = process.env.CRT_ARN ? process.env.CRT_ARN : "";
+    const bucketName = process.env.BUCKET_NAME;
+    const domainName = process.env.Domain_NAME ? process.env.Domain_NAME : "";
+    const hostedZoneId = process.env.HOST_ZONE ? process.env.HOST_ZONE : "";
+    const zoneName = process.env.ZONE_NAME ? process.env.ZONE_NAME : "";
+    const recordName = process.env.RECORD_NAME ? process.env.RECORD_NAME : "";
+
+    const hebrewBucket = new s3.Bucket(this, 'hebrewBucket', {
+      bucketName: bucketName,
+    });
+
+
+    const cert = crt.Certificate.fromCertificateArn(
+      this,
+      'heb-crt',
+      certArn,
+    )
+
+
+    const hebrewDistribution = new cf.Distribution(this, 'hebrewDist', {
+      httpVersion: cf.HttpVersion.HTTP2,
+      enableIpv6: true,
+      domainNames: [domainName],
+      defaultRootObject: 'index.html',
+      certificate: cert,
+      defaultBehavior: {
+        origin: new origins.S3Origin(hebrewBucket, {
+          originPath: "/inedx.html",
+        }),
+        viewerProtocolPolicy: cf.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+    });
+
+    const bucketDeployment = new depl.BucketDeployment(
+      this,
+      'mainBucketDeployment',
+      {
+        sources: [depl.Source.asset(path.join(__dirname, '../../dist/hebrew-app/browser/'))],
+        destinationBucket: hebrewBucket,
+        distribution: hebrewDistribution,
+        prune: true,
+      }
+    );
+
+    const route53Zone = route.HostedZone.fromHostedZoneAttributes(this, "route", {
+      hostedZoneId: hostedZoneId,
+      zoneName: zoneName,
+    })
+
+    const record = new route.CnameRecord(this, "route-record", {
+      zone: route53Zone,
+      deleteExisting: true,
+      recordName: recordName,
+      domainName: hebrewDistribution.domainName
+    })
+  }
+
+}
+
---
+++ aws-hebrew-infra/test/aws-hebrew-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsHebrewInfra from '../lib/aws-hebrew-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-hebrew-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsHebrewInfra.AwsHebrewInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

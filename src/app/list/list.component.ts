67d97a63d9da490b521921cf983878cbe906090a
tonyjafar <thaer.m.jafar@gmail.com>
2024-07-17 14:01:40 +0200

    remove unused code

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -4,19 +4,6 @@ import { NgxPaginationModule } from 'ngx-pagination';
 import { CommonModule } from '@angular/common';
 
 
-
-
-
-const totalItems = words.length;
-const pageSize = 20;
-let currentPage = 0;
-
-let items = words.slice(currentPage - 1, pageSize);
-export function pageChanged(event: any) {
-  currentPage = event.page;
-  items = words.slice(currentPage - 1, pageSize);
-}
-
 @Component({
   selector: 'app-list',
   standalone: true,
@@ -26,7 +13,5 @@ export function pageChanged(event: any) {
 })
 export class ListComponent {
   wordList = words;
-  currentPage = currentPage;
-  pageChanged = pageChanged;
   p: any;
 }

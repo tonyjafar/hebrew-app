a99e61744d5e8f9f20d2319cfec0da1800bc8875
tony <thaer.m.jafar@gmail.com>
2025-02-11 18:58:21 +0100

    fix pagination style

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -10,7 +10,16 @@ import { CommonModule } from '@angular/common';
     templateUrl: './list.component.html',
     styleUrl: './list.component.css'
 })
+
+
+
 export class ListComponent {
   wordList = words;
-  p: any;
+  p: number = 1; // Current page
+
+  onPageChange(event: number) {
+    this.p = event;
+    // Scroll to top when page changes
+    window.scrollTo(0, 0);
+  }
 }

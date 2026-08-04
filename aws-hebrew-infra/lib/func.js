03c4d1d2e53163ab0f15adc382e7e6560a8d75e9
tony <thaer.m.jafar@gmmail.com>
2026-08-04 08:39:57 +0200

    add username and password

--- aws-hebrew-infra/lib/func.js
+++ aws-hebrew-infra/lib/func.js
@@ -1,6 +1,31 @@
+import cf from 'cloudfront';
+const kvsHandle = cf.kvs();
+
+
 function handler(event) {
+
     var request = event.request;
+    var headers = request.headers;
+    let authUser = "";
+    let authPass = "";
+  
+    // basic auth
+    authUser = await kvsHandle.get("authUser");
+    authPass = await kvsHandle.get("authPass");
+    var authString = 'Basic ' + (authUser + ':' + authPass).toString('base64');
     var uri = request.uri;
+    if (typeof headers.authorization == 'undefined' || headers.authorization.value != authString) {
+        var response = {
+            statusCode: 401,
+            statusDescription: 'Unauthorized',
+            headers: {
+                'www-authenticate': {
+                    'value' : 'Basic realm="Enter credentials."'
+                }
+            }
+        };
+        return response;
+    }
 
 
     if (uri == '/') {
